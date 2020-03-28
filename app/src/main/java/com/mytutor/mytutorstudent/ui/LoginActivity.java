6b7dda9154f1f6a06c4376e716f7328402ec65f5
arunachalam <arunachalam@think42labs.com>
2020-03-28 18:09:22 +0530

    Changed designs and integrated dashboard

--- app/src/main/java/com/mytutor/mytutorstudent/ui/LoginActivity.java
+++ app/src/main/java/com/mytutor/mytutorstudent/ui/LoginActivity.java
@@ -1,45 +1,48 @@
-package com.mytutor.mytutorstudent;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+package com.mytutor.mytutorstudent.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCanceledListener;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.mytutor.mytutorstudent.R;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText ed_email, ed_password;
-    private Button btn_signIn, btn_register;
-    private String mail,password;
+    private Button btn_signIn;
+    private TextView btn_register;
+    private String mail, password;
     private FirebaseFirestore db;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        auth = FirebaseAuth.getInstance();
         declaration();
 
         btn_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this,RegisterAct.class));
+                startActivity(new Intent(LoginActivity.this, RegisterAct.class));
             }
         });
 
@@ -48,9 +51,25 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 mail = ed_email.getText().toString();
                 password = ed_password.getText().toString();
-                if(!(TextUtils.isEmpty(mail) || TextUtils.isEmpty(password)))
-                    getData(mail, password);
-                else
+                if (!(TextUtils.isEmpty(mail) || TextUtils.isEmpty(password))) {
+                    auth.signInWithEmailAndPassword(mail, password).
+                            addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, "Email / password invalid try again", Toast.LENGTH_SHORT).show();
+                                    }
+
+
+                                }
+
+
+                            });
+
+
+                } else
                     Toast.makeText(LoginActivity.this, "Fill the details", Toast.LENGTH_SHORT).show();
             }
         });
@@ -58,30 +77,28 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
-    public void declaration(){
+    public void declaration() {
         ed_email = findViewById(R.id.ed_MailId);
         ed_password = findViewById(R.id.ed_Pass);
         btn_signIn = findViewById(R.id.btn_signIn);
         btn_register = findViewById(R.id.btn_register);
-
         db = FirebaseFirestore.getInstance();
-
     }
 
-    public void getData(String pmail, String ppass){
+    public void getData(String pmail, String ppass) {
         db.collection("student")
-                .whereEqualTo("Email",pmail)
-                .whereEqualTo("Password",ppass)
+                .whereEqualTo("Email", pmail)
+                .whereEqualTo("Password", ppass)
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if(task.isSuccessful()){
-                            for(QueryDocumentSnapshot document : task.getResult()){
-                                Log.d("TAG",document.getId() + " => " + document.getData());
-                                Toast.makeText(LoginActivity.this,"Signed in Successfully!", Toast.LENGTH_SHORT).show();
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAG", document.getId() + " => " + document.getData());
+                                Toast.makeText(LoginActivity.this, "Signed in Successfully!", Toast.LENGTH_SHORT).show();
                             }
-                        }else{
+                        } else {
                             Toast.makeText(LoginActivity.this, "Account does Not Exists", Toast.LENGTH_SHORT).show();
                         }
                     }
